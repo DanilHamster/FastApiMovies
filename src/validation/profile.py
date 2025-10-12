b3ca006ea720d305a7f885c8860963d6ad2b0751
Hamster <dtaranenko137@gmail.com>
2025-10-12 20:12:27 +0300

    implement mypy and fix other file

--- src/validation/profile.py
+++ src/validation/profile.py
@@ -8,7 +8,7 @@ from fastapi import UploadFile
 from database.models.accounts import GenderEnum
 
 
-def validate_name(name: str):
+def validate_name(name: str) -> None:
     if re.search(r'^[A-Za-z]*$', name) is None:
         raise ValueError(f'{name} contains non-english letters')
 
