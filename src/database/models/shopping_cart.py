a8fbadcc8b264b3bd1aa46f5e4d450d428bb606a
Glego <glego200323@gmail.com>
2025-10-14 01:33:53 +0300

    Fix isort.

--- src/database/models/shopping_cart.py
+++ src/database/models/shopping_cart.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from sqlalchemy import Integer, ForeignKey, DateTime, UniqueConstraint, func
+
+from sqlalchemy import DateTime, ForeignKey, Integer, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from database import Base
 
 
