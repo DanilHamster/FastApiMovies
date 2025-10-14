1d8da9fff670a39a42902c2dfc3d853ea66d1cbd
Hamster <dtaranenko137@gmail.com>
2025-10-14 22:10:13 +0300

    implemented celery task

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -2,13 +2,16 @@ from celery.schedules import crontab
 
 from celery_conf import celery_app
 
-# from src.tasks import notify_user
+from src.tasks import task_for_clean_tokens
 
-# """Example"""
-# @celery_app.on_after_configure.connect
-# def setup_periodic_task(sender, **kwargs):
-#     sender.add_periodic_task(
-#         10.0,
-#         notify_user.s(),
-#         name="Test"
-#     )
+
+"""Example"""
+
+
+@celery_app.on_after_configure.connect
+def setup_periodic_task(sender, **kwargs):
+    sender.add_periodic_task(
+        30.0,
+        task_for_clean_tokens.s(),
+        name="Test"
+    )
