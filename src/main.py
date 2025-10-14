d75e07c110507b90f054202f9340db5bbf0a6ced
Tamerlan <egtamerlan@gmail.com>
2025-10-14 18:50:55 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routes.movies import router as movies_router
 
 from config.settings import API_VERSION_PREFIX
 from routes import accounts
@@ -8,4 +9,6 @@ app = FastAPI(
     description="Description of project"
 )
 
+api_version_prefix = "/api/v1"
 app.include_router(accounts.router, prefix=f"{API_VERSION_PREFIX}/accounts", tags=["User"])
+app.include_router(movies_router, prefix=api_version_prefix)
--- src/routes/movies.py
+++ src/routes/movies.py
@@ -3,7 +3,7 @@ from math import ceil
 from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.session_sqlite import get_session
+from database import get_db
 from repositories.movies import MovieRepository
 from schemas.movies import MovieCreate, MovieDetail, MovieListResponse, MovieUpdate
 
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/movies", tags=["Movies"])
 async def list_movies(
     page: int = Query(1, ge=1),
     per_page: int = Query(10, ge=1, le=100),
-    session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_db),
 ) -> MovieListResponse:
     repo = MovieRepository(session)
     skip = (page - 1) * per_page
@@ -36,7 +36,7 @@ async def list_movies(
 @router.get("/{movie_id}", response_model=MovieDetail)
 async def read_movie(
     movie_id: int = Path(..., ge=1),
-    session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_db),
 ) -> MovieDetail:
     repo = MovieRepository(session)
     movie = await repo.get_by_id(movie_id)
@@ -48,7 +48,7 @@ async def read_movie(
 @router.post("/", response_model=MovieDetail, status_code=201)
 async def create_new_movie(
     movie: MovieCreate,
-    session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_db),
 ) -> MovieDetail:
     repo = MovieRepository(session)
     return await repo.create(movie)
@@ -58,7 +58,7 @@ async def create_new_movie(
 async def update_existing_movie(
     movie_id: int = Path(..., ge=1),
     movie_data: MovieUpdate = Body(),
-    session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_db),
 ) -> MovieDetail:
     repo = MovieRepository(session)
     updated_movie = await repo.update(movie_id, movie_data)
@@ -70,7 +70,7 @@ async def update_existing_movie(
 @router.delete("/{movie_id}", status_code=204)
 async def remove_movie(
     movie_id: int = Path(..., ge=1),
-    session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_db),
 ) -> None:
     repo = MovieRepository(session)
     deleted = await repo.delete(movie_id)
