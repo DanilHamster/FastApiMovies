aeac57360eeaeb55c3afe49aa1559f1bb0f52453
reinvvay <reinvvay@gmail.com>
2025-10-14 10:33:42 +0300

    feature: user endpoints base implementation

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -1,14 +1,14 @@
-from celery.schedules import crontab
-
-from celery_conf import celery_app
-
-# from src.tasks import notify_user
-
-# """Example"""
-# @celery_app.on_after_configure.connect
-# def setup_periodic_task(sender, **kwargs):
-#     sender.add_periodic_task(
-#         10.0,
-#         notify_user.s(),
-#         name="Test"
-#     )
+# from datetime import datetime, timezone
+# from database import async_session_maker
+#
+# @celery.task
+# async def cleanup_expired_activation_tokens():
+#     async with async_session_maker() as db:
+#         now = datetime.now(timezone.utc)
+#         stmt = select(ActivationTokenModel).where(ActivationTokenModel.expires_at < now)
+#         result = await db.execute(stmt)
+#         expired_tokens = result.scalars().all()
+#
+#         for token in expired_tokens:
+#             await db.delete(token)
+#         await db.commit()
