8452cefb088986c424767e0c40e05760bef00dda
Glego <glego200323@gmail.com>
2025-10-15 02:04:27 +0300

    Stripe first commit.

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,6 +31,12 @@ class BaseAppSettings(BaseSettings):
     S3_STORAGE_SECRET_KEY: str = os.getenv("MINIO_ROOT_PASSWORD", "some_password")
     S3_BUCKET_NAME: str = os.getenv("MINIO_STORAGE", "theater-storage")
 
+    stripe_secret_key: str = os.getenv("STRIPE_SECRET_KEY")
+    stripe_publishable_key: str = os.getenv("STRIPE_PUBLISHABLE_KEY")
+    stripe_webhook_secret: str = os.getenv("STRIPE_WEBHOOK_SECRET")
+
+    app_base_url: str = os.getenv("BASE_URL")
+
     @property
     def S3_STORAGE_ENDPOINT(self) -> str:
         return f"http://{self.S3_STORAGE_HOST}:{self.S3_STORAGE_PORT}"
---
+++ src/routes/payments.py
@@ -0,0 +1,80 @@
+from fastapi import APIRouter, Depends, Request, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from config.settings import Settings
+from services import stripe_service, payment_service
+from database.session_postgresql import get_postgresql_db
+from database.models.payments import PaymentStatus
+from decimal import Decimal
+
+from database import Base
+
+router = APIRouter(prefix="/payments", tags=["payments"])
+
+class CheckoutRequest(Base):
+    order_id: int
+    amount: Decimal
+    currency: str = "usd"
+
+@router.post("/checkout")
+async def create_checkout(
+    req: CheckoutRequest,
+    db: AsyncSession = Depends(get_postgresql_db),
+    #current_user = Depends(get_current_user),
+):
+    metadata = {
+        #"user_id": str(current_user.id),
+        "order_id": str(req.order_id),
+    }
+    session = stripe_service.create_checkout_session(
+        amount=float(req.amount),
+        currency=req.currency,
+        success_url=Settings.app_base_url + "/payments/success",
+        cancel_url=Settings.app_base_url + "/payments/cancel",
+        metadata=metadata,
+    )
+
+    await payment_service.create_payment(
+        db=db,
+        #user_id=current_user.id,
+        order_id=req.order_id,
+        amount=req.amount,
+        external_payment_id=session.id,
+        status=PaymentStatus.canceled,
+    )
+
+    return {"checkout_url": session.url, "session_id": session.id}
+
+@router.post("/webhook")
+async def stripe_webhook(
+    request: Request, db: AsyncSession = Depends(get_postgresql_db)
+):
+    payload = await request.body()
+    sig_header = request.headers.get("stripe-signature")
+
+    event = stripe_service.verify_signature_and_construct_event(payload, sig_header)
+
+    if event.type == "checkout.session.completed":
+        session = event.data.object
+        external_id = session["id"]
+        payment = await payment_service.get_payment_by_external_id(db, external_id)
+        if payment:
+            await payment_service.update_payment_status(db, payment, PaymentStatus.successful)
+
+    elif event.type == "checkout.session.expired" or event.type == "checkout.session.canceled":
+        session = event.data.object
+        external_id = session["id"]
+        payment = await payment_service.get_payment_by_external_id(db, external_id)
+        if payment:
+            await payment_service.update_payment_status(db, payment, PaymentStatus.canceled)
+
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"received": True})
+
+@router.get("/success")
+async def pay_success():
+    return {"message": "Payment succeeded"}
+
+@router.get("/cancel")
+async def pay_cancel():
+    return {"message": "Payment canceled"}
