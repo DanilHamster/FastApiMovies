bb2fba5a87b1bd2f638a09d57cf3853a27692793
Hamster <dtaranenko137@gmail.com>
2025-10-16 00:02:48 +0300

    fix test

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -123,7 +123,7 @@ class MovieDetail(MovieBase):
     certification: CertificationResponse
     like_count: int = 0
     dislike_count: int = 0
-    comments: List[CommentResponse]
+    comments: List[CommentResponse] = None
 
 
 class MovieList(BaseModel):
