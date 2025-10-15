b32656538a647aec1b7421484b5985823195268d
Tamerlan <egtamerlan@gmail.com>
2025-10-15 17:21:15 +0200

    add search and ordering

--- src/filters/filter_movies.py
+++ src/filters/filter_movies.py
@@ -2,10 +2,9 @@ from typing import Optional
 from fastapi_filter.contrib.sqlalchemy import Filter
 from database.models.movies import MovieModel
 
-
 class MovieFilter(Filter):
-
     name__ilike: Optional[str] = None
+    description__ilike: Optional[str] = None
     year__gte: Optional[int] = None
     year__lte: Optional[int] = None
     imdb__gte: Optional[float] = None
@@ -14,8 +13,8 @@ class MovieFilter(Filter):
     price__lte: Optional[float] = None
     certification_id: Optional[int] = None
     genre_id: Optional[int] = None
-    director_id: Optional[int] = None
-    star_id: Optional[int] = None
+    director_name__ilike: Optional[str] = None
+    star_name__ilike: Optional[str] = None
     order_by: Optional[str] = None
 
     class Constants(Filter.Constants):
