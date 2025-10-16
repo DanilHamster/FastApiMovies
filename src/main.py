3e8d4d38c467ca02a696ddc8319bbb4a6c116b5c
reinvvay <reinvvay@gmail.com>
2025-10-16 11:26:36 +0300

    fix: main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-
-from routes import accounts, admin_orders_router, orders_router, shopping_cart, profiles
+from config.settings import API_VERSION_PREFIX
+from routes import (
+    accounts,
+    admin_orders_router,
+    orders_router,
+    shopping_cart,
+    profiles,
+)
 from routes.movies import router as movies_router
 
 app = FastAPI(title="Movies Api", description="Description of project")
 
-api_version_prefix = "/api/v1"
-
-app.include_router(
-    accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["User"]
-)
-app.include_router(
-    profiles.router, prefix=api_version_prefix, tags=["Profile"]
-)
-app.include_router(movies_router, prefix=api_version_prefix)
-app.include_router(orders_router, prefix=api_version_prefix)
-app.include_router(admin_orders_router, prefix=api_version_prefix)
-app.include_router(shopping_cart.router, prefix=api_version_prefix)
+app.include_router(accounts.router, prefix=f"{API_VERSION_PREFIX}/accounts", tags=["User"])
+app.include_router(profiles.router, prefix=API_VERSION_PREFIX, tags=["Profile"])
+app.include_router(movies_router, prefix=API_VERSION_PREFIX)
+app.include_router(orders_router, prefix=API_VERSION_PREFIX)
+app.include_router(admin_orders_router, prefix=API_VERSION_PREFIX)
+app.include_router(shopping_cart.router, prefix=API_VERSION_PREFIX)
